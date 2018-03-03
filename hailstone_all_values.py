fb0dcccecb5745ba9f3f9f9af1a3ebb10c5bebbc
BitNibbleByte <mn15347@students.mcpasd.k12.wi.us>
2018-03-03 13:56:40 -0600

    Allow saving of dictionary

--- hailstone_all_values.py
+++ hailstone_all_values.py
@@ -1,4 +1,15 @@
-hailstone_archive = {}
+import json
+
+main_archive = {}
+
+class Status():
+    def __init__(self, index, gns, tn, archive):
+        self.index = index
+        self.greatest_num_steps = gns
+        self.top_num = tn
+        self.hailstone_archive = archive
+
+status = Status(2, 1, 2, main_archive)
 
 def isEven(num):
     if(num%2 == 0):
@@ -16,29 +27,60 @@ def analyze(num):
     count = 0
     hail_order = []
     while(num != 1):
-        if num in hailstone_archive:
-            count = count + hailstone_archive[num]
+        if num in status.hailstone_archive:
+            count = count + status.hailstone_archive[num]
             break
         elif num%2 == 0:
+            hail_order.append(num)
             num = applyEven(num)
             count = count + 1
         else:
+            hail_order.append(num)
             num = applyOdd(num)
             count = count + 1
     if not len(hail_order) == 0:
         for i in range(0,len(hail_order)):
-            hailstone_archive[hail_order[i]] = count
+            status.hailstone_archive[hail_order[i]] = count
     return count
 
-index = 2
-greatest_num_steps = 1
-top_num = 2
+print("Q to quite and save dictionary to file in data directory")
 
-#for now use control-c to break loop
-while(True):
-    count = analyze(index)
-    if(count > greatest_num_steps):
-        greatest_num_steps = count
-        top_num = index
-        print(greatest_num_steps, ", ", top_num)
-    index = index + 1
+def loop():
+    while(True):
+        count = analyze(status.index)
+        if(count > status.greatest_num_steps):
+            status.greatest_num_steps = count
+            status.top_num = status.index
+            print("Num: ", status.top_num, ", Steps: ", status.greatest_num_steps)
+        status.index = status.index + 1
+
+def main():
+    try:
+        loop()
+    except KeyboardInterrupt:
+        Uin = input("Enter a command (q to quit and d to show dict length and continue): ")
+        if(Uin == "q"):
+            pass
+        elif(Uin == "d"):
+            print(len(status.hailstone_archive))
+            UinPrime = input("Continue (Y/N): ")
+            if(UinPrime == "Y"):
+                main()
+            else:
+                pass
+
+main()
+
+
+main_input = input("Would you like to save the hailstone_dictionary that was created? (Y/N): ")
+
+if(main_input == "Y"):
+    print("Saving dictionary to data directory...")
+
+    #save data
+    with open('data/data.json', 'w') as fp:
+        json.dump(status.hailstone_archive, fp)
+
+    print("Data saved.")
+else:
+    print("Done")
